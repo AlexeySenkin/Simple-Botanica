8739c62014e8785e743dd3a6d1b621053faa9f35
MuzafarovAA <artmuz@mail.ru>
2023-03-25 13:12:07 +0500

    Добавил каркас модульного проекта, произвел первичную настройку

---
+++ module.webapi/src/main/java/ru/botanica/WebApiApplication.java
@@ -0,0 +1,13 @@
+package ru.botanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiApplication.class, args);
+    }
+
+}
