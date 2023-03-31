b68675940f9034e7ca12dd3d5d0aa7c85d63b900
Maria Kuzmina <bigcat82@gmail.com>
2023-03-31 15:47:15 +0300

    plants.html пагинация добавлена, функции получения данных с фронта для списка растений

--- module.frontend-service/src/main/resources/static/Plants/plants.js
+++ module.frontend-service/src/main/resources/static/Plants/plants.js
@@ -1,5 +1,5 @@
 angular.module('Simple-Botanica-app')
-    .controller('plants-controller', function ($http, $rootScope, $scope, botanicaConfig, api_version) {
+    .controller('plants-controller', function ($http, $rootScope, $scope, botanicaConfig, api_version, paginatonFactory) {
         const plantsPath = 'localhost:3010/api/' + api_version.api_v + 'plants'
         $scope.plantListCallPlace = botanicaConfig.plantListCallPlace;
 
@@ -8,22 +8,40 @@ angular.module('Simple-Botanica-app')
             , {photo: 'img/db/Spatifilum.jpg', name: 'Спатифилум', id: 3}
             , {photo: 'img/db/Alocasia.jpg', name: 'Алоказия', id: 4}
             , {photo: 'img/db/Spatifilum.jpg', name: 'Спатифилум', id: 5}
-            , {photo: 'img/db/Spatifilum.jpg', name: 'Спатифилум', id: 6}
+            , {photo: 'img/db/Scindapsus.jpg', name: 'Сциндапсус', id: 6}
             , {photo: 'img/db/Spatifilum.jpg', name: 'Спатифилум', id: 7}
             , {photo: 'img/db/Spatifilum.jpg', name: 'Спатифилум', id: 8}]
 
 
         $scope.getPlants = function () {
-            $http.get(plantsPath).then(function sucsessCallback(response) {
-                    $scope.plantsPage = response.data();
+            $http.get(plantsPath, {
+                params: {
+                    name: $scope.plantNameFilter,
+                    page: $scope.page
+                }
+            }).then(function successCallback(response) {
+                    $scope.plantsPage = response.data.content;
+                    $scope.pageNumbersArray = paginatonFactory.generateRangeForPagination(response.data.number,
+                        8, response.data.totalPages - 1);
+                    $scope.totalPages = response.data.totalPages;
+                    $scope.currentPage = response.data.number;
+                    $scope.totalItems = response.data.totalElements;
                 },
                 function errorCallback(reason) {
                     $scope.plantsPage = plantsArray;
+                    $scope.totalPages = 4;
+                    $scope.currentPage = 2;
+                    $scope.totalItems = 30;
                 })
-            alert($scope.plantNameFilter)
         }
 
         $scope.showPlantDetails = function (plantId) {
+            $http.get(plantsPath + '/' + plantId).then(function successCallback(response) {
+                    $scope.plant = response.data;
+                },
+                function errorCallback(reason) {
+
+                });
             alert(plantId);
         }
 
@@ -31,6 +49,5 @@ angular.module('Simple-Botanica-app')
             return true;
         }
         $scope.getPlants();
-
 
     })
