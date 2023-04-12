1b9e7d1ecfc245a735b1ecc06a4494864b5a4b3a
MuzafarovAA <artmuz@mail.ru>
2023-04-13 00:32:50 +0500

    Добавил возврат ResponseEntity и логирование в UserController

--- module.user-service/src/main/java/ru/botanica/controllers/UserController.java
+++ module.user-service/src/main/java/ru/botanica/controllers/UserController.java
@@ -2,11 +2,11 @@ package ru.botanica.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.botanica.entities.users.UserDto;
 import ru.botanica.services.UserService;
 
 @RestController
@@ -22,8 +22,14 @@ public class UserController {
      * @return Пользователь
      */
     @GetMapping("/user/{id}")
-    public UserDto findById(@PathVariable long id) {
-        return userService.findById(id);
+    public ResponseEntity<?> findById(@PathVariable long id) {
+        try {
+            log.debug("Получение пользователя по id: {}", id);
+            return ResponseEntity.ok().body(userService.findById(id));
+        } catch (Exception e) {
+            log.error("Ошибка получения пользователя по id: {}", id);
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     /**
@@ -33,8 +39,14 @@ public class UserController {
      * @return Идентификатор
      */
     @GetMapping("/user")
-    public Long findIdByUserName(@RequestParam("username") String userName) {
-        return userService.findIdByUserName(userName);
+    public ResponseEntity<?> findIdByUserName(@RequestParam("username") String userName) {
+        try {
+            log.debug("Получение идентификатора пользователя по имени: {}", userName);
+            return ResponseEntity.ok().body(userService.findIdByUserName(userName));
+        } catch (Exception e) {
+            log.error("Ошибка идентификатора пользователя по имени: {}", userName);
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
 }
