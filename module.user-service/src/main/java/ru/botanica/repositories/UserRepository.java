98a38f3ef2f079d347a49c2a29f3d88c70882754
alexsenkin <senkinalexey@mail.ru>
2023-04-18 14:17:54 +0300

    Доработка user-service: исправил конфликт версий, соединил работу Артура и мою

--- module.user-service/src/main/java/ru/botanica/repositories/UserRepository.java
+++ module.user-service/src/main/java/ru/botanica/repositories/UserRepository.java
@@ -1,8 +1,9 @@
-package ru.botanica.entities.users;
+package ru.botanica.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ru.botanica.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
